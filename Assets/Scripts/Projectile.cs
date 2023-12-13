9c7c7a2c5baff129580707ecf6155dad555351f8
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-12-13 13:08:56 +0700

    Update Projectile cs and add layer enemny
    update projectile cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     float speed = 10;
+    public LayerMask collisionMask;
 
    public void setSpeed(float newSpeed)
     {
         speed = newSpeed;
     }
 
-
     void Update()
     {
-        transform.Translate (Vector3.forward * Time.deltaTime * speed); 
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollision(moveDistance);
+        transform.Translate (Vector3.forward * moveDistance); 
+    }
+
+    void CheckCollision (float moveDistance)
+    {
+        Ray ray = new Ray (transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast (ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {}
+    }
+    
+    void onHitObject(RaycastHit hit)
+    {
+        GameObject.Destroy (gameObject);
     }
 }
