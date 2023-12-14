3903b41d3b6183123a1bf464f7139aabdb764af2
Laxcod <90342920+Laxcod@users.noreply.github.com>
2023-12-14 10:43:48 +0700

    update iddamageable cs and projectile cs
    update skrip iddamageable and projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,6 +8,19 @@ public class Projectile : MonoBehaviour
     float damage = 1;
     public LayerMask collisionMask;
 
+    float lifetime = 3;
+
+   void Start()
+   {
+        Destroy (gameObject, lifetime);
+
+        Collider[] initialCollision = Physics.OverlapSphere (transform.position, .1f, collisionMask);
+        if (initialCollision.Length > 0)
+        {
+            OnHitObject (initialCollision [0], transform.position);
+        }
+   } 
+
    public void setSpeed(float newSpeed)
     {
         speed = newSpeed;
@@ -27,16 +40,16 @@ public class Projectile : MonoBehaviour
 
         if (Physics.Raycast (ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
         {
-            OnHitObject (hit);
+            OnHitObject (hit.collider, hit.point);
         }
     }
     
-    void OnHitObject(RaycastHit hit)
+    void OnHitObject(Collider c, Vector3 hitPoint)
     {
-      IDamageable damageableObject = hit.collider.GetComponent<IDamageable> ();
+      IDamageable damageableObject = c.GetComponent<IDamageable> ();
       if (damageableObject !=null)
       {
-        damageableObject.TakeHit (damage, hit);
+        damageableObject.TakeHit (damage, hitPoint, transform.forward);
       }
         GameObject.Destroy (gameObject);
     }
